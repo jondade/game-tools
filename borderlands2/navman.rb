0de10a5b725fa2fd0244fe4534f3dde23c777dd0
Jonathan Dade <jonathandade@gmail.com>
2017-01-09 09:20:07 +0000

    Some move towards a class structure.

--- borderlands2/navman.rb
+++ borderlands2/navman.rb
@@ -1,61 +1,94 @@
 #!/usr/bin/env ruby
 
-require_relative 'route.rb'
+require_relative 'lib/route.rb'
 require 'getoptlong'
 require 'erb'
-
-def print_maps
-  list_maps.sort.each do |name|
-    puts name
-  end
-  exit 0
-end
+require 'pry'
 
 def print_help
   proc_name = File.basename(__FILE__)
   renderer = ERB.new(File.read(File.dirname(__FILE__) + '/lib/help.erb'))
-  puts res = renderer.result(binding)
-
-#   puts "Usage: #{File.basename(__FILE__)} [-h, --help] [-l, --list-maps] " \ 
-#   "[-s, --start LOCATION] [-f, --finish LOCATION]"
-#   puts
-#   puts '  [-h, --help] Displays this message.'
-#   puts '  [-l, --list-maps] Displays a list of known maps in Borderlands 2.'
-#   puts '  [-s, --start] The name of the map you are currently on.'
-#   puts '  [-f, --finish] The name of the map you want routes to.'
-#   puts
-#   puts 'NOTE: When using start and finish arguments BOTH are required.'
-#   puts
+  res = renderer.result(binding)
+  puts res
   exit 0
 end
 
+def print_maps(router, map)
+  if map.nil? || map == ''
+    router.list_maps.sort.each do |name|
+      puts name
+    end
+  else
+    puts "#{map} has exits:"
+    router.list_map(map).sort.each do |exit|
+      puts "\t-#{exit}"
+    end
+  end
+end
+
+def find_route(router, start, finish)
+  if start.nil? || finish.nil?
+    puts 'Insufficient locations to find routes.'
+    puts
+    print_help
+  else
+    routes = router.find_route(start, finish)
+    router.print_routes(routes)
+  end
+end
+
+def add_route(router, start, finish)
+  if start.nil? || finish.nil?
+    puts 'Insufficient locations to add new route.'
+    puts "start: #{start} | finish: #{finish}"
+    puts
+    print_help
+  else
+    router.add_route(start, finish)
+  end
+end
+
+#
+# Main code starts here
+#
+router = Router.new
+next_step = nil
 start = nil
 finish = nil
 
 opts = GetoptLong.new(
   ['--start', '-s', GetoptLong::REQUIRED_ARGUMENT],
   ['--finish', '-f', GetoptLong::REQUIRED_ARGUMENT],
-  ['--list-maps', '-l', GetoptLong::NO_ARGUMENT],
-  ['--help', '-h', GetoptLong::NO_ARGUMENT]
+  ['--list-maps', '-l', GetoptLong::OPTIONAL_ARGUMENT],
+  ['--help', '-h', GetoptLong::NO_ARGUMENT],
+  ['--add', '-a', GetoptLong::NO_ARGUMENT],
+  ['--route', '-r', GetoptLong::NO_ARGUMENT]
 )
 
 opts.each do |opt, arg|
   case opt
   when '--help'
     print_help
-  when '--list-maps'
-    print_maps
   when '--start'
     start = arg
   when '--finish'
     finish = arg
+  when '--add'
+    next_step = 'add_route'
+  when '--route'
+    next_step = 'find_route'
+  when '--list-maps'
+    next_step = 'print_maps'
+    start = arg
   end
 end
 
-if start.nil? || finish.nil?
-  puts 'Insufficient locations to find routes.'
-  puts
-  print_help
+if next_step == 'add_route'
+  add_route(router, start, finish)
+elsif next_step == 'find_route'
+  find_route(router, start, finish)
+elsif next_step == 'print_maps'
+  print_maps(router, start)
 else
-  find_route(start, finish)
+  print_help
 end
